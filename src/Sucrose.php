9e74360c8da8099f8dce5d78e8158eecc08242c8
recycledbeans <matt@recycledbeans.com>
2018-01-11 13:30:59 -0500

    Updates setDeletedAttribute method to only mark deleted = 1 if value of deleted is a valid datetime

--- src/Sucrose.php
+++ src/Sucrose.php
@@ -11,7 +11,16 @@ class Sucrose extends Model {
 
     const UPDATED_AT = 'date_modified';
 
+    const DELETED_AT = 'deleted';
+
     public $incrementing = false;
+
+    public function setDeletedAttribute()
+    {
+        if(DateTime::createFromFormat('Y-m-d H:i:s', $this->attribute['deleted']) !== false) {
+            $this->attribute['deleted'] = 1;
+        }
+    }
 
     public function save(array $options = []) {
         if ( empty($this->id) ) {
